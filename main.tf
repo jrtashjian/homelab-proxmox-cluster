04815a874927d68ad3119d19cbb86df8ba4e4856
JR Tashjian <hello@jrtashjian.com>
2024-02-06 00:11:17 -0500

    Add force flag to proxmox_virtual_environment_certificate resource

--- main.tf
+++ main.tf
@@ -12,4 +12,6 @@ resource "proxmox_virtual_environment_certificate" "int_jrtashjian_com" {
 
   certificate = trimspace(var.int_jrtashjian_com_cert)
   private_key = trimspace(var.int_jrtashjian_com_key)
+
+  force = true
 }
