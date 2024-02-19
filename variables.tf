caaa32098be75be3a0ad0ad7876d6a749a2ba99b
JR Tashjian <hello@jrtashjian.com>
2024-02-19 17:56:37 -0500

    Remove unused variable "node_datastore" in variables.tf

--- variables.tf
+++ variables.tf
@@ -3,12 +3,6 @@ variable "node_name" {
   type        = string
 }
 
-variable "node_datastore" {
-  description = "proxmox node default datastore"
-  type        = string
-  default     = "local-zfs"
-}
-
 variable "int_jrtashjian_com_cert" {
   description = "int.jrtashjian.com certificate"
   type        = string
