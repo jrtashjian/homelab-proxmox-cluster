6e1e89784fff03f4a2804de23f2e809549155dea
JR Tashjian <hello@jrtashjian.com>
2026-03-28 02:29:44 -0400

    Add Terraform-managed role and user for automation in Proxmox

--- main.tf
+++ main.tf
@@ -126,3 +126,46 @@ resource "proxmox_virtual_environment_metrics_server" "influxdb" {
     ignore_changes = [influx_token]
   }
 }
+
+resource "proxmox_virtual_environment_role" "terraform-automation" {
+  role_id = "terraform-automation"
+
+  privileges = [
+    "Datastore.Allocate",
+    "Datastore.AllocateSpace",
+    "Datastore.AllocateTemplate",
+    "Datastore.Audit",
+    "Pool.Allocate",
+    "Pool.Audit",
+    "SDN.Use",
+    "Sys.Audit",
+    "Sys.Console",
+    "Sys.Modify",
+    "VM.Allocate",
+    "VM.Audit",
+    "VM.Clone",
+    "VM.Config.CDROM",
+    "VM.Config.Cloudinit",
+    "VM.Config.CPU",
+    "VM.Config.Disk",
+    "VM.Config.HWType",
+    "VM.Config.Memory",
+    "VM.Config.Network",
+    "VM.Config.Options",
+    "VM.Migrate",
+    "VM.Monitor",
+    "VM.PowerMgmt",
+  ]
+}
+
+resource "proxmox_virtual_environment_user" "terraform_automation" {
+  acl {
+    path      = "/"
+    propagate = true
+    role_id   = proxmox_virtual_environment_role.terraform-automation.role_id
+  }
+
+  comment  = "Managed by Terraform"
+  password = var.terraform_password
+  user_id  = var.terraform_username
+}
