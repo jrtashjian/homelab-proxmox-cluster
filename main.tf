ac1986a372180bf4e163c025bb6a3b9fa03462f5
JR Tashjian <hello@jrtashjian.com>
2026-03-28 02:28:40 -0400

    Add NFS storage resource and daily backup job configuration

--- main.tf
+++ main.tf
@@ -86,3 +86,27 @@ resource "proxmox_virtual_environment_cluster_firewall_security_group" "ssh-serv
     log     = "nolog"
   }
 }
+
+# Backup NFS storage.
+resource "proxmox_virtual_environment_storage_nfs" "nfs_backup" {
+  id     = "nfs-backup"
+  server = "192.168.10.11"
+  export = "/mnt/kraken_z2_primary/Backups/Proxmox_Virtual_Machines"
+
+  content = ["backup"]
+
+  backups {
+    max_protected_backups = 5
+    keep_daily            = 7
+  }
+}
+
+resource "proxmox_backup_job" "daily_backup" {
+  id             = "daily-backup"
+  schedule       = "21:00" # Every day at 9:00 PM
+  storage        = proxmox_virtual_environment_storage_nfs.nfs_backup.id
+  all            = true
+  mode           = "snapshot"
+  compress       = "zstd"
+  notes_template = "{{guestname}}"
+}
