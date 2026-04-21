f4e56d2431cb34bf6c690484b595ce91f90a4871
JR Tashjian <hello@jrtashjian.com>
2026-04-21 14:06:27 -0400

    Refactor datastore handling in Terraform files; remove unused resource and update datastore_id assignments

--- terraform/files.tf
+++ terraform/files.tf
@@ -1,30 +1,29 @@
+data "proxmox_virtual_environment_datastores" "datastores" {
+  for_each  = toset(data.proxmox_virtual_environment_nodes.nodes.names)
+  node_name = each.value
+}
+
+locals {
+  # Build a clean map: node_name => { iso, snippets, vztmpl }
+  datastore = {
+    for node in data.proxmox_virtual_environment_nodes.nodes.names : node => {
+      iso      = one([for ds in data.proxmox_virtual_environment_datastores.datastores[node].datastores : ds.id if contains(ds.content_types, "iso")])
+      snippets = one([for ds in data.proxmox_virtual_environment_datastores.datastores[node].datastores : ds.id if contains(ds.content_types, "snippets")])
+      vztmpl   = one([for ds in data.proxmox_virtual_environment_datastores.datastores[node].datastores : ds.id if contains(ds.content_types, "vztmpl")])
+    }
+  }
+}
 
 resource "proxmox_virtual_environment_file" "debian_container_template" {
   for_each  = toset(data.proxmox_virtual_environment_nodes.nodes.names)
   node_name = each.value
 
   content_type = "vztmpl"
-  datastore_id = "local"
+  datastore_id = local.datastore[each.value].vztmpl
 
   source_file {
     path = "http://download.proxmox.com/images/system/debian-13-standard_13.1-2_amd64.tar.zst"
   }
-}
-
-resource "proxmox_virtual_environment_download_file" "debian_cloud_image" {
-  for_each  = toset(data.proxmox_virtual_environment_nodes.nodes.names)
-  node_name = each.value
-
-  content_type = "iso"
-  datastore_id = "local"
-
-  url = "https://cloud.debian.org/images/cloud/trixie/20260402-2435/debian-13-genericcloud-amd64-20260402-2435.qcow2"
-
-  checksum           = "df0f1b09350658b0f42b83337682f36d7b821ea3a438d8257906e25350d2f92b0dcce810b0e6958887fb5210fd48625b8330339673df1ded127ffc0a969c12e5"
-  checksum_algorithm = "sha512"
-
-  file_name = "debian-13-genericcloud-amd64.img"
-  overwrite = false
 }
 
 resource "proxmox_virtual_environment_file" "debian_vendor_config" {
@@ -32,10 +31,10 @@ resource "proxmox_virtual_environment_file" "debian_vendor_config" {
   node_name = each.value
 
   content_type = "snippets"
-  datastore_id = "local"
+  datastore_id = local.datastore[each.value].snippets
 
   source_raw {
-    data      = <<-EOF
+    data = <<-EOF
     #cloud-config
     package_update: true
     packages:
